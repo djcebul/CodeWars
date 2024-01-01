d911327cc4b8b5dc22e57fe512f1cae5870e04aa
djcebul <76882442+djcebul@users.noreply.github.com>
2024-01-02 00:39:12 +0100

    Initials

---
+++ src/E5_Initials.java
@@ -0,0 +1,12 @@
+public class E5_Initials {
+    public static void main(String[] args) {
+        System.out.println( abbrevName("Daniel Cebula"));
+    }
+    public static String abbrevName(String name) {
+        String a, b;
+        a = String.valueOf(name.charAt(0)).toUpperCase();
+        b = String.valueOf(name.charAt((name.indexOf(" ")) + 1)).toUpperCase();
+
+        return a + "." + b;
+    }
+}

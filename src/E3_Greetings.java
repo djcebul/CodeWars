8ecf414d6b23618840c3b6fc7708e625dd8956c0
djcebul <76882442+djcebul@users.noreply.github.com>
2024-01-02 00:11:34 +0100

    greeting

---
+++ src/E3_Greetings.java
@@ -0,0 +1,10 @@
+/*Make a function that will return a greeting statement
+that uses an input; your program should return,
+        "Hello, <name> how are you doing today?".*/
+
+public class E3_Greetings {
+    public static String greet(String name)
+    {
+        return "Hello, " + name + " how are you doing today?";
+    }
+}

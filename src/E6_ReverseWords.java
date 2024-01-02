02a9f696af87f1057fc1f843f10ea3a70323fd2a
djcebul <76882442+djcebul@users.noreply.github.com>
2024-01-02 21:12:03 +0100

    E6 2nd version

--- src/E6_ReverseWords.java
+++ src/E6_ReverseWords.java
@@ -9,21 +9,44 @@ Examples
 
 public class E6_ReverseWords {
     public static void main(String[] args) {
-        System.out.println(reverseWords("This is an example!"));
+        //System.out.println(reverseWords("This is an example!"));
+        String example1 = reverseWords("This is an example!");
+        String example2 = reverseWords("double  spaces");
+
+        System.out.println(example1); // Oczekiwane wyjście: "sihT si na !elpmaxe"
+        System.out.println(example2); // Oczekiwane wyjście: "elbuod  secaps"
 
     }
+
+//    public static String reverseWords(final String original)
+//    {
+//        var reverse = new StringBuilder();
+//        var r = new StringBuilder();
+//        String[] words = original.split("\\s+");
+//        for (int i = 0; i < words.length; i++) {
+//            r = new StringBuilder(words[i]).reverse();
+//            if (i == words.length-1) reverse.append(r);
+//            else reverse.append(r).append(" ");
+//        }
+//        return reverse.toString();
 
     public static String reverseWords(final String original)
     {
-        var reverse = new StringBuilder();
-        var r = new StringBuilder();
-        String[] words = original.split("\\s+");
-        for (int i = 0; i < words.length; i++) {
-            r = new StringBuilder(words[i]).reverse();
-            if (i == words.length-1) reverse.append(r);
-            else reverse.append(r).append(" ");
-        }
-        return reverse.toString();
-    }
+        String[] array = original.split(" ");
 
+        if(array.length == 0)
+            return original;
+
+
+        int i = 0;
+        for(String string : array){
+            array[i] = new StringBuilder(string).reverse().toString();
+            i++;
+        }
+
+        return String.join(" ",array);
+    }
 }
+
+
+
